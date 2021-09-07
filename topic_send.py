5325b6afaff0adf6ccbde5c4c60341315a80713b
Pat Riehecky <riehecky@fnal.gov>
2021-09-07 10:32:20 -0500

    blaken

--- topic_send.py
+++ topic_send.py
@@ -10,7 +10,27 @@ queue = Queue("", exchange=exchange)
 with Connection(broker_url) as conn:
     producer = conn.Producer(serializer="json")
 
-    producer.publish("message for example", routing_key="example", exchange=exchange, maybe_declare=[queue])
-    producer.publish("message for example.text", routing_key="example.text", exchange=exchange, maybe_declare=[queue])
-    producer.publish("message for test.code.example", routing_key="test.code.example", exchange=exchange, maybe_declare=[queue])
-    producer.publish("message for test.code.example.thing", routing_key="test.code.example.thing", exchange=exchange, maybe_declare=[queue])
+    producer.publish(
+        "message for example",
+        routing_key="example",
+        exchange=exchange,
+        maybe_declare=[queue],
+    )
+    producer.publish(
+        "message for example.text",
+        routing_key="example.text",
+        exchange=exchange,
+        maybe_declare=[queue],
+    )
+    producer.publish(
+        "message for test.code.example",
+        routing_key="test.code.example",
+        exchange=exchange,
+        maybe_declare=[queue],
+    )
+    producer.publish(
+        "message for test.code.example.thing",
+        routing_key="test.code.example.thing",
+        exchange=exchange,
+        maybe_declare=[queue],
+    )
