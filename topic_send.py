7f4406698f27c02a2e888b4ce8e3d0da41c3be43
Pat Riehecky <riehecky@fnal.gov>
2021-09-07 10:31:41 -0500

    Added topic examples

---
+++ topic_send.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from kombu import Connection, Exchange, Queue
+
+from celeryconfig import broker_url
+
+exchange = Exchange("example_topic_exchange", "topic")
+queue = Queue("", exchange=exchange)
+
+with Connection(broker_url) as conn:
+    producer = conn.Producer(serializer="json")
+
+    producer.publish("message for example", routing_key="example", exchange=exchange, maybe_declare=[queue])
+    producer.publish("message for example.text", routing_key="example.text", exchange=exchange, maybe_declare=[queue])
+    producer.publish("message for test.code.example", routing_key="test.code.example", exchange=exchange, maybe_declare=[queue])
+    producer.publish("message for test.code.example.thing", routing_key="test.code.example.thing", exchange=exchange, maybe_declare=[queue])
